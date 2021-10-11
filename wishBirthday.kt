df0f29e6655fa3f72bc64d5458a5bf0561a8c109
Md. Nurul Islam Talukder Nahid <61370261+nitalukder@users.noreply.github.com>
2021-10-11 18:44:52 +0600

    Create wishBirthday.kt

---
+++ wishBirthday.kt
@@ -0,0 +1,21 @@
+fun main() {
+    
+    val age = 21 * 365
+    val name = "Nahid"
+    
+    println("Happy Birthday, ${name}!")
+    
+    // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    
+    // This prints an empty line.
+    println("")
+
+    println("You are already ${age} days old, ${name}!")
+    println("${age} days old is the very best age to celebrate!")
+}
